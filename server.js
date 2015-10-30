defb37ae4718df74eb81974308575f4ff007e994
Doublelinee <csbackstep@gmail.com>
2015-10-30 21:41:57 +0700

    test code schema

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ var model = require('./models/iot.js')
 mongoose.connect('mongodb://localhost/db')
 
 var app = express()
+app.post('/',function(req,res){
+    var obj = new model(req.body)
+    obj.save(function (err, obj) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        res.send(obj)
+      }
+    })
+})
 
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
