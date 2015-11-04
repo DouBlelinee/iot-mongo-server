3077396d837f04663428d9db2d43e428a8ffef94
Doublelinee <csbackstep@gmail.com>
2015-11-05 01:22:08 +0700

    test query data

--- public/app.js
+++ public/app.js
@@ -1,14 +1,18 @@
 angular.module('iot', [])
       .controller('IotController', function ($http) {
         var scope = this
-       scope.insert = function(value){
-       	console.log(value)
+       scope.insert = function (value) {
         $http.post('/api/iot', value)
           .then(function success (response) {
             alert('Success')
           }, function error (response) {
             alert(response.data.message)
-        })
+            	})
       }
-      })
+   		scope.query  = function() {
+ 			$http.get('/api/iot').success(function (response) {
+    	console.log(response)
+    })
 
+}
+})
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,6 @@
 var express = require('express')
 var router = express.Router()
-
 var iot = require('../models/iot')
-
 iot.methods(['get', 'put', 'post', 'delete'])
 iot.register(router, '/iot')
-
 module.exports = router
