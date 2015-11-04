6d551b605a6873c7ba7894914a4a142d6bb34fac
Doublelinee <csbackstep@gmail.com>
2015-11-04 10:36:10 +0700

    insert fail

---
+++ index.js
@@ -0,0 +1,38 @@
+var unirest = require('unirest')
+
+var addr = process.argv[2]
+var port = process.argv[3]
+
+var url = 'http://' + addr + ':' + port
+
+for (var i = 0; i < 10; i++) {
+  postIOT(i)
+}
+
+function postIOT (id) {
+  var timestapm = Math.floor(Date.now() / 1000)
+  for (var i = 100; i >= 0; i--) {
+    post(id, timestapm)
+    timestapm = timestapm - 3600
+  }
+}
+
+function post (id, timestapm) {
+  var data = {
+    'timestamp': timestapm,
+    'iot_id': id,
+    'temperature': ran(0, 60),
+    'relative_humidity': ran(1, 100)
+  }
+  console.log(data)
+  unirest.post(url)
+    .type('json')
+    .send(data)
+    .end(function (response) {
+      console.log(response.body)
+    })
+}
+
+function ran (start, end) {
+  return Math.floor((Math.random() * end) + start)
+}
