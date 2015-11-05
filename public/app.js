2d02de09615ee1c69cd4ab9e554ad7f6f5c72f9e
Doublelinee <csbackstep@gmail.com>
2015-11-06 00:57:09 +0700

    test graph

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 angular.module('iot', [])
       .controller('IotController', function ($http) {
         var scope = this
-       scope.insert = function (value) {
+    scope.insert = function (value) {
         $http.post('/api/iot', value)
           .then(function success (response) {
             alert('Success')
@@ -23,7 +23,7 @@ angular.module('iot', [])
  		scope.toTime = function(date){
     return moment(date).format('MMMM Do YYYY , h:mm:ss a')
 }
-scope.delete = function(id,index){
+	scope.delete = function(id,index){
         console.log(id)
         $http.delete('/api/iot/'+id)
           .success(function(data) {
@@ -32,6 +32,55 @@ scope.delete = function(id,index){
           .error(function(data) {
             console.log('Error: ' + data)
           })
+    }
+
+    scope.graph = function(){
+    	console.log('eeeee')
+      $http.get('/api/iot')
+              .then(function success (response) {
+         
+                  var data = {
+                              labels: ["January", "February", "March", "April", "May", "June", "July"],
+                              datasets: [
+                                  {
+                                      label: "temperature",
+                                      fillColor: "rgba(255,0,0,0.2)",
+                                      strokeColor: "rgba(255,0,0,1)",
+                                      pointColor: "rgba(255,0,0,1)",
+                                      pointStrokeColor: "#fff",
+                                      pointHighlightFill: "#fff",
+                                      pointHighlightStroke: "rgba(220,220,220,1)",
+                                      data: []
+                                  },
+                                  {
+                                      label: "relative_humidity",
+                                      fillColor: "rgba(69,187,91,0.2)",
+                                      strokeColor: "rgba(69,187,91,1)",
+                                      pointColor: "rgba(69,187,91,1)",
+                                      pointStrokeColor: "#fff",
+                                      pointHighlightFill: "#fff",
+                                      pointHighlightStroke: "rgba(151,187,205,1)",
+                                      data: []
+                                  }
+                              ]
+                          };
+
+               var ctx = document.getElementById("c").getContext("2d")
+               var myLineChart = new Chart(ctx).Line(data);
+
+               
+                  for(var i = 0;i<response.data.length;i++){
+                    if (response.data[i].iot_id==0){
+                         myLineChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,scope.toTime(response.data[i].timestamp))
+                       }
+                   
+                }
+               
+
+              }, function error (response) {
+                alert(response.data.message)
+              }) 
+      
     }
 })
        
