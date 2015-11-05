9c1a2d8d2c8e1b789f4f68d8da8b41e3d384c6ce
Doublelinee <csbackstep@gmail.com>
2015-11-05 23:31:34 +0700

    edit again

--- public/app.js
+++ public/app.js
@@ -5,9 +5,12 @@ angular.module('iot', [])
         $http.post('/api/iot', value)
           .then(function success (response) {
             alert('Success')
+            scope.query()
+            value.iot_id = value.temperature = value.relative_Humidity = []
           }, function error (response) {
             alert(response.data.message)
             	})
+
       }
    	scope.query = function () {
  			$http.get('/api/iot').success(function (response) {
@@ -16,6 +19,10 @@ angular.module('iot', [])
     	
 
     })
-
+ 		}
+ 		scope.toTime = function(date){
+    return moment(date).format('MMMM Do YYYY , h:mm:ss a')
 }
+
 })
+  
