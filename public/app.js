8b65c921584b647d4e598826c204b74f392dc496
Doublelinee <csbackstep@gmail.com>
2015-11-06 01:45:29 +0700

    bar graph

--- public/app.js
+++ public/app.js
@@ -36,43 +36,38 @@ angular.module('iot', [])
 
     scope.graph = function(){
       $http.get('/api/iot')
-              .then(function success (response) {
-              	console.log('eeeee')
-         
+              .then(function success (response) {         
                   var data = {
                               labels: [],
                               datasets: [
-                                  {
-                                      label: "temperature",
-                                      fillColor: "rgba(255,0,0,0.2)",
-                                      strokeColor: "rgba(255,0,0,1)",
-                                      pointColor: "rgba(255,0,0,1)",
-                                      pointStrokeColor: "#fff",
-                                      pointHighlightFill: "#fff",
-                                      pointHighlightStroke: "rgba(220,220,220,1)",
-                                      data: []
-                                  },
-                                  {
-                                      label: "relative_humidity",
-                                      fillColor: "rgba(69,187,91,0.2)",
-                                      strokeColor: "rgba(69,187,91,1)",
-                                      pointColor: "rgba(69,187,91,1)",
-                                      pointStrokeColor: "#fff",
-                                      pointHighlightFill: "#fff",
-                                      pointHighlightStroke: "rgba(151,187,205,1)",
-                                      data: []
-                                  }
+                                 {
+            label: "temperature",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: []
+        },
+        {
+            label: "relative_humidity",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: []
+        }
                               ]
                           }
 
                var ctx = document.getElementById("iot").getContext("2d")
-               console.log(ctx)
-               var myLineChart = new Chart(ctx).Line(data);
+               var myBarChart = new Chart(ctx).Bar(data)
+
+  
 
                
                   for(var i = 0;i<response.data.length;i++){
                     if (response.data[i].iot_id==0){
-                         myLineChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,scope.toTime(response.data[i].timestamp))
+                         myBarChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,scope.toTime(response.data[i].timestamp))
                        }
                    
                 }
